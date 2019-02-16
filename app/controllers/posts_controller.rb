6f509ac4fe474ee641bccde6540773e07bbf5be4
Judy McGoldrick <jjeanius@verizon.net>
2019-02-16 22:09:42 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,8 +31,9 @@ class PostsController < ApplicationController
     @post.update(params.require(:post))
     redirect_to post_path(@post)
   end
-
+if para
   def edit
+    
     @post = Post.find(params[:id])
   end
 
