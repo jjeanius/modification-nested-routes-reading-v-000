23863a29409b3a9d9a14cc74fb718547cb43e4b7
Judy McGoldrick <jjeanius@verizon.net>
2019-02-16 22:14:22 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
   end
 
   def edit
-    if params[:author_id]  
+    if params[:author_id]
       author = Author.find_by(id: params[:author_id])
       if author.nil?
         redirect_to authors_path, alert: "Author not found."
